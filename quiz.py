05537944def447f5eaf68428678d53b0874a9c6a
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-08-10 14:56:26 +0100

    input values quiz app

---
+++ quiz.py
@@ -0,0 +1,10 @@
+languages = ["python", "c", "c++", "java"]
+
+user_input = input("Which is your favourite programming language?")
+
+if user_input in languages:
+    print("Oshey, Idan gan gan")
+elif user_input == "html":
+    print("Who be this one 😁")
+else:
+    print("You never ready")
