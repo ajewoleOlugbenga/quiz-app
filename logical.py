fa58750905adea6a86f2bd8df3d16a073a190e01
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-08-11 05:06:01 +0100

    and, or, not operators

---
+++ logical.py
@@ -0,0 +1,29 @@
+"""
+hidden operators
+NOT
+OR
+AND
+"""
+"""
+instead of using elif we can use or to check for the other variables
+"""
+my_bool = True
+my_num = 43
+
+"""#not logical operator
+if not my_bool:
+    print("It's not True")
+elif my_bool:
+    print("Yes it is True")
+else:
+    print("Whatever")
+"""
+"""
+#or operator --instead of elif statement
+
+if not my_bool or my_num == 42:
+    print("The statement pass")
+else:
+    print("The statement reject")
+    """
+#and operator s to check if the two variable are true
